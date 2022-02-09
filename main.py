62fcbcaca92ebe0f8077ddda2dd0729552ede1a9
Tooraj Taraz <tooraj.info@gmail.com>
2022-02-09 19:54:38 +0330

    Adding constant datas such as path and book names

--- main.py
+++ main.py
@@ -2,3 +2,32 @@ import cv2
 import numpy as np
 from time import gmtime, strftime
 from math import pi, atan2
+
+#NEEDED DATA
+TWO_PI = 2 * pi
+
+#Remember to change this to the correct path
+data_folder_path = '$HOME/Documents/ItLeavesATrail/data/'
+
+books = [
+    'Angels and Demons', 
+    # 'Anne of Green Gables & Anne of Avonlea', 
+    'David Copperfield', 
+    # 'Dracula', 
+    # 'Pickwick Papers', 
+    # 'To Kill a Mockingbird',
+    # 'Tom Sawyer and Huckleberry Finn',
+    # 'Twenty Thousand Leagues Under the Sea',
+    # 'Twilight - New Moon',
+    # 'Twilight - Eclipse'
+]
+
+format_extentions = [
+    '.jpg',
+    '.mp4'
+]
+
+base_clips = [
+    '0.MOV',
+    '1.MOV'
+]
